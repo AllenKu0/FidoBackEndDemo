07e4a574fbcd5687014e4cda47eaa7127986af38
uuko <omkkqq@gmail.com>
2022-05-19 19:31:32 +0800

    "changle place"

--- src/main/java/com/example/springboot/controller/login/UserController.java
+++ src/main/java/com/example/springboot/controller/login/UserController.java
@@ -12,27 +12,23 @@ import com.example.springboot.request.WelcomRequest;
 import com.example.springboot.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.yubico.webauthn.*;
 import com.yubico.webauthn.data.*;
-import com.yubico.webauthn.exception.AssertionFailedException;
 import com.yubico.webauthn.exception.RegistrationFailedException;
-import io.swagger.annotations.ApiOperation;
-import jdk.internal.icu.text.NormalizerBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 //@RequestMapping("/api/users")
@@ -44,9 +40,19 @@ public class UserController {
 
     private RelyingParty relyingParty;
 
+    //
+    private final Cache<String, PublicKeyCredentialCreationOptions> registrationCache;
+    private final Cache<String,  AssertionRequest> loginCache;
 
-    UserController(RelyingParty relyingPary) {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private AuthenticatorRepository authenticatorRepository;
+    UserController(RelyingParty relyingPary, Cache<String, PublicKeyCredentialCreationOptions> registrationCache, Cache<String, AssertionRequest> loginCache) {
         this.relyingParty = relyingPary;
+        this.registrationCache = registrationCache;
+        this.loginCache = loginCache;
     }
     @PostMapping("/register")
     @ResponseBody
@@ -69,7 +75,27 @@ public class UserController {
     public String newAuthRegistration(
             @RequestBody User user
     ){
-        return userService.newAuthRegistration(user,relyingParty);
+//        return userService.newAuthRegistration(user,relyingParty);
+        Optional<User> existingUser = userRepository.findByHandle(user.getHandle());
+        if (existingUser.isPresent()) {
+            UserIdentity userIdentity = user.toUserIdentity();
+            StartRegistrationOptions registrationOptions = StartRegistrationOptions.builder()
+                    .user(userIdentity)
+                    .build();
+
+//            HttpSession session
+
+//            registrationCache.setAttribute(userIdentity.getDisplayName(), registration);
+            try {
+                PublicKeyCredentialCreationOptions registration = relyingParty.startRegistration(registrationOptions);
+                this.registrationCache.put(userIdentity.getName(), registration);
+                return registration.toCredentialsCreateJson();
+            } catch (JsonProcessingException e) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error processing JSON.", e);
+            }
+        } else {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "User " + user.getEmail() + " does not exist. Please register.");
+        }
     }
 
     @PostMapping("/finishauth")
@@ -78,13 +104,38 @@ public class UserController {
             @RequestBody FinishAuthRequest request
     ) {
         try {
-            userService.finishAuth(request, relyingParty);
+//            userService.finishAuth(request, relyingParty);
+            Optional<User> user = findByEmail(request.getUsername());
+            if (user.isPresent()) {
+                PublicKeyCredentialCreationOptions requestOptions = (PublicKeyCredentialCreationOptions) registrationCache.getIfPresent(user.get().getEmail());
+                this.registrationCache.invalidate(user.get().getEmail());
+
+                if (requestOptions != null) {
+                    PublicKeyCredential<AuthenticatorAttestationResponse, ClientRegistrationExtensionOutputs> pkc =
+                            PublicKeyCredential.parseRegistrationResponseJson(request.getCredential());
+                    FinishRegistrationOptions options = FinishRegistrationOptions.builder()
+                            .request(requestOptions)
+                            .response(pkc)
+                            .build();
+                    RegistrationResult result = relyingParty.finishRegistration(options);
+                    Authenticator savedAuth = new Authenticator(result, pkc.getResponse(), user.get(), request.getCredname());
+                    authenticatorRepository.save(savedAuth);
+                } else {
+                    throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Cached request expired. Try to register again!");
+                }
+
+
+            }
             return new ModelAndView("redirect:/login", HttpStatus.SEE_OTHER);
         } catch (RegistrationFailedException e) {
             throw new ResponseStatusException(HttpStatus.BAD_GATEWAY, "Registration failed.", e);
         } catch (IOException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Failed to save credenital, please try again!", e);
         }
+    }
+
+    public Optional<User> findByEmail(String userName) {
+        return userRepository.findByEmail(userName);
     }
 
     @PostMapping("/login")
