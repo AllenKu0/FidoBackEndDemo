feecb2e93f71c9fe300f288a4951693ea5e95989
uuko <omkkqq@gmail.com>
2022-05-23 16:46:36 +0800

    "lessone post api "

--- src/main/java/com/example/springboot/repository/UserRepository.java
+++ src/main/java/com/example/springboot/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.springboot.repository;
 
 import com.example.springboot.entity.User;
 import com.yubico.webauthn.data.ByteArray;
+import io.swagger.models.auth.In;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,5 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByHandle(ByteArray handle);
+    Optional<User> findById(Long id);
 
 }
--- src/main/java/com/example/springboot/service/CourseSelectionService.java
+++ src/main/java/com/example/springboot/service/CourseSelectionService.java
@@ -4,19 +4,38 @@ import com.example.springboot.entity.CourseSelection;
 import com.example.springboot.entity.Lesson;
 import com.example.springboot.entity.User;
 import com.example.springboot.repository.CourseSelectionRepository;
+import com.example.springboot.repository.LessonRepository;
+import com.example.springboot.repository.UserRepository;
 import com.example.springboot.request.CourseSelectionRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CourseSelectionService {
     @Autowired
     private CourseSelectionRepository courseSelectionRespority;
 
-    public void saveCourse(CourseSelectionRequest courseSelectionRequest){
-       CourseSelection courseSelection=new CourseSelection(new Lesson(courseSelectionRequest.getLesson_id()),
-               new User(courseSelectionRequest.getUser_id()));
-        courseSelectionRespority.save(courseSelection);
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveCourse(CourseSelectionRequest courseSelectionRequest) throws Exception {
+
+        Optional<User> user=userRepository.findById(courseSelectionRequest.getUser_id());
+        Optional<Lesson> lesson=lessonRepository.findById(courseSelectionRequest.getLesson_id());
+        if (lesson.isPresent() && user.isPresent()){
+            CourseSelection courseSelection=new CourseSelection(lesson.get(),
+                    user.get());
+            courseSelectionRespority.save(courseSelection);
+        }
+        else {
+            throw new Exception("no find");
+        }
+
 
     }
 //    public ListLessonResponse getAllLesson(){
