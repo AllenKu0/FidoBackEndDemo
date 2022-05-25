3a9b926c6f88c66a03150418d863011f53ef5a3a
Allen <eacf2469@gmail.com>
2022-05-25 20:04:11 +0800

    swagger 新增

---
+++ src/main/java/com/example/springboot/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.example.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableAsync
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    public WebConfig() {
+        super();
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
