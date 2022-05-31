fb49a1bc5b8167b820b251efb0ccb9e589a55051
uuko <omkkqq@gmail.com>
2022-05-31 16:02:05 +0800

    "log "

--- src/main/java/com/example/springboot/config/CorsFilter.java
+++ src/main/java/com/example/springboot/config/CorsFilter.java
@@ -21,9 +21,11 @@ public class CorsFilter implements Filter {
         String requestOrigin = request.getHeader("Origin");
         System.out.println("Cors : " + requestOrigin);
         if (requestOrigin.equals(URL)){
+            System.out.println("Access-Control-Allow-Origin : " + requestOrigin);
             response.addHeader("Access-Control-Allow-Origin", requestOrigin);
         }
         else {
+            System.out.println("Access-Control-Allow-Origin : http://123.241.245.130:8080" );
             response.addHeader("Access-Control-Allow-Origin", "http://123.241.245.130:8080");
         }
 //        response.setHeader("Access-Control-Allow-Origin","http://localhost:8080,http://123.241.245.130:8080/");
