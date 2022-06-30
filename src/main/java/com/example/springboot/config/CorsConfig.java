690d3721f0d06f08a124be2126fbe2293be6e1d4
AllenKu0 <eacf2469@gmail.com>
2022-06-30 12:37:28 +0800

    cros 嘗試修正

--- src/main/java/com/example/springboot/config/CorsConfig.java
+++ src/main/java/com/example/springboot/config/CorsConfig.java
@@ -2,15 +2,15 @@ package com.example.springboot.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-@Configuration
+//@Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true)
-                .maxAge(3600)
-                .allowedHeaders("*");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns("*")
+//                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowCredentials(true)
+//                .maxAge(3600)
+//                .allowedHeaders("*");
+//    }
 }
--- src/main/java/com/example/springboot/config/CorsFilter.java
+++ src/main/java/com/example/springboot/config/CorsFilter.java
@@ -10,51 +10,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(filterName = "CorsFilter ")
-@Configuration
-public class CorsFilter implements Filter {
-    private static final String URL = "http://localhost:8080"; // URL
-    private static final String OTHER_URL = "http://123.241.245.130:8080"; // OTHER URL
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        String requestOrigin = request.getHeader("Origin");
-        System.out.println("Cors : " + requestOrigin);
-        if (requestOrigin==null){
-            System.out.println("Access-Control-Allow-Origin NULL: http://123.241.245.130:8080" );
-            response.addHeader("Access-Control-Allow-Origin", "http://123.241.245.130:8080");
-//            System.out.println("Access-Control-Allow-Origin NULL: http://192.168.68.102:8080" );
-//            response.addHeader("Access-Control-Allow-Origin","http://192.168.68.102:8080");
-        }
-        else if (requestOrigin.equals(URL)){
-            System.out.println("Access-Control-Allow-Origin : " + requestOrigin);
-            response.addHeader("Access-Control-Allow-Origin", requestOrigin);
-        }
-        else {
-            System.out.println("Access-Control-Allow-Origin : http://123.241.245.130:8080" );
-            response.addHeader("Access-Control-Allow-Origin", "http://123.241.245.130:8080");
-//            System.out.println("Access-Control-Allow-Origin : http://192.168.68.102:8080" );
-//            response.addHeader("Access-Control-Allow-Origin","http://192.168.68.102:8080");
-        }
-//        response.setHeader("Access-Control-Allow-Origin","http://localhost:8080,http://123.241.245.130:8080/");
-        response.setHeader("Access-Control-Allow-Origin","*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        chain.doFilter(servletRequest, res);
-    }
-
-    private void setAccessControlAllowOrigin(HttpServletResponse response,
-                                             ServletRequest request) {
-        String requestUrl = request.getRemoteAddr() + ":" + request.getRemotePort();
-        System.out.println("Cors : " + requestUrl);
-        if (OTHER_URL.equals(requestUrl)) {
-            response.setHeader("Access-Control-Allow-Origin", OTHER_URL);
-        } else {
-            response.setHeader("Access-Control-Allow-Origin", URL);
-        }
-    }
+//@WebFilter(filterName = "CorsFilter ")
+//@Configuration
+public class CorsFilter  {
+//    private static final String URL = "http://localhost:8080"; // URL
+//    private static final String OTHER_URL = "http://123.241.245.130:8080"; // OTHER URL
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+//        HttpServletResponse response = (HttpServletResponse) res;
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        String requestOrigin = request.getHeader("Origin");
+//        System.out.println("Cors : " + requestOrigin);
+//        if (requestOrigin==null){
+//            System.out.println("Access-Control-Allow-Origin NULL: http://123.241.245.130:8080" );
+//            response.addHeader("Access-Control-Allow-Origin", "http://123.241.245.130:8080");
+////            System.out.println("Access-Control-Allow-Origin NULL: http://192.168.68.102:8080" );
+////            response.addHeader("Access-Control-Allow-Origin","http://192.168.68.102:8080");
+//        }
+//        else if (requestOrigin.equals(URL)){
+//            System.out.println("Access-Control-Allow-Origin : " + requestOrigin);
+//            response.addHeader("Access-Control-Allow-Origin", requestOrigin);
+//        }
+//        else {
+//            System.out.println("Access-Control-Allow-Origin : http://123.241.245.130:8080" );
+//            response.addHeader("Access-Control-Allow-Origin", "http://123.241.245.130:8080");
+////            System.out.println("Access-Control-Allow-Origin : http://192.168.68.102:8080" );
+////            response.addHeader("Access-Control-Allow-Origin","http://192.168.68.102:8080");
+//        }
+////        response.setHeader("Access-Control-Allow-Origin","http://localhost:8080,http://123.241.245.130:8080/");
+//        response.setHeader("Access-Control-Allow-Credentials", "true");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//        chain.doFilter(servletRequest, res);
+//    }
+//
+//    private void setAccessControlAllowOrigin(HttpServletResponse response,
+//                                             ServletRequest request) {
+//        String requestUrl = request.getRemoteAddr() + ":" + request.getRemotePort();
+//        System.out.println("Cors : " + requestUrl);
+//        if (OTHER_URL.equals(requestUrl)) {
+//            response.setHeader("Access-Control-Allow-Origin", OTHER_URL);
+//        } else {
+//            response.setHeader("Access-Control-Allow-Origin", URL);
+//        }
+//    }
 }
